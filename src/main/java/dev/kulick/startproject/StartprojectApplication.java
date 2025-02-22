12f4b4f684e62cf18b0df2a32afce1b916ad415e
kulickk <lilrik1328@gmail.com>
2025-02-22 15:31:53 +0500

    initial

---
+++ src/main/java/dev/kulick/startproject/StartprojectApplication.java
@@ -0,0 +1,13 @@
+package dev.kulick.startproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kulick/startproject/StartprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kulick.startproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
